3c6ebdd7b65bcf265c9baebee1a58ecf6d0876c0
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2018-09-21 13:45:46 +0600

    commit for test

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+let testButton = document.getElementById('test-button');
+
+
+testButton.addEventListener('click', function(){
+    testClickFunction();});
+
+function testClickFunction(){
+    alert('button clicked!');
+}
