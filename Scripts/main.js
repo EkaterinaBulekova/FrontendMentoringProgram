80298c536ad07012d62ab2303ea448793faef261
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2018-10-01 10:04:54 +0600

    home page with empty table

--- Scripts/main.js
+++
@@ -1,9 +0,0 @@
-let testButton = document.getElementById('test-button');
-
-
-testButton.addEventListener('click', function(){
-    testClickFunction();});
-
-function testClickFunction(){
-    alert('button clicked!');
-}
