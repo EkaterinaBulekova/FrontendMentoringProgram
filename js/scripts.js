0e786d381f0521c8a20b35ea5e37f813ee5ca56d
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2018-10-22 14:59:21 +0600

    changed to html5 structered project, added pagenation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 let advFiltersButton = document.getElementById("aFilterButton");
 advFiltersButton.addEventListener('click', function(){
     let advFiltersPanel = document.getElementById("aFilterPanel");
-    advFiltersPanel.style = "display:block"
+    advFiltersPanel.style =  (advFiltersPanel.style["display"] == "block")
+        ? "display: none" 
+        : "display: block";
 }); 
+
 function AjaxLoad(LoadUrl, options, callback) {
     var fullurl = LoadUrl
     console.log(options)
@@ -15,51 +18,67 @@ function AjaxLoad(LoadUrl, options, callback) {
     }
     console.log(fullurl)
  
-    $.ajax({
+    var requestresult = $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         url: fullurl,
-        success: function (result) {
-            callback(result);
+        success: function (result) { 
+            callback(result, requestresult.getResponseHeader('x-total-count'), requestresult.getResponseHeader('link'));
         },
-        error: function (data) {
+        error: function () {
             alert("Извините произошла непредвиденная ошибка обратитись пожалуйста к администратору сайта.");
         }
     });
 }
 
-function LoadTable(data){
-    var tableHead = document.getElementById('OrderTableHead');
-    var tableRef = document.getElementById('OrderTableBody');
-    tableRef.innerHTML = "";    
+function LoadTable(data, count, linkstr){
+   var tableHead = document.getElementById('OrderTableHead');
+   tableHead.innerHTML = "";    
     let newRow = tableHead.insertRow(0);
     let j =0;
-    for(var propertyName in data[0]) {
+    for(let propertyName in data[0]) {
         
         let newCell  = newRow.insertCell(j);
         let newText  = document.createTextNode(propertyName);
         newCell.appendChild(newText);
         j++;
     }
-    for(let i = 0; i < data.length; i++){
+
+    var tableRef = document.getElementById('OrderTableBody');
+    tableRef.innerHTML = "";
+     for(let i = 0; i < data.length; i++){
         let item = data[i]
         let newRow   = tableRef.insertRow(i);          
         let j =0;
-        for(var propertyName in item) {
+        for(let propertyName in item) {
             let newCell  = newRow.insertCell(j);
             let newText  = document.createTextNode(item[propertyName]);
             newCell.appendChild(newText);
             j++;
         }       
     }
+ 
+    var pageButtons = document.getElementById('pageButtons');
+    pageButtons.innerHTML = "";
+    var links = linkstr.split(', ')   
+    for(let i = 0; i < links.length; i++){
+        let button = document.createElement('button');
+        let linkParam = links[i].split('; ');
+        let buttonName = linkParam[1].slice(5, linkParam[1].length-1);
+        let buttonUrl = linkParam[0].slice(1, linkParam[0].length-1);
+        button.innerHTML = buttonName;
+        button.onclick = function(){
+            AjaxLoad(buttonUrl, null, LoadTable);
+        }; 
+        pageButtons.appendChild(button);        
+    } 
 }
 
 $(document).ready(function() {
     url = "http://localhost:3000/orders?";
     var options = [{Name:"_page", Value:"1"},{Name :"_limit", Value:"5"}];
     AjaxLoad(url, options, LoadTable)
- //   AjaxLoad(url, [{Name:"_page", Value:"1"},{Name :"_limit", Value:"5"}], LoadTable)
- });
+});
  
 
